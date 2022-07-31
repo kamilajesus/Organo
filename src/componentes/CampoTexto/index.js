0dd5ff82390bac5da792ebb0c2d2163a1b682975
Kamila Jesus <kamilaalves727@gmail.com>
2022-07-31 20:33:46 -0300

    new commit

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,16 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+  const placeholder = `${props.placeholder}...`
+
+  return(
+    <div className="campo-texto">
+      <label>
+        {props.label}
+      </label>
+      <input placeholder={placeholder}/>
+    </div>
+  )
+}
+
+export default CampoTexto
