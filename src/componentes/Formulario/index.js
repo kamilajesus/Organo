f04bd759b332d0dc17ce64275c3605e0e19dc03d
Kamila Jesus <kamilaalves727@gmail.com>
2022-08-03 22:11:00 -0300

    Montando os times

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import { useState } from 'react';
 
 const Formulario = (props) => {
 
-const times = [
-      'Programação',
-      'Front-end',
-      'Data Science',
-      'Devops',
-      'UX e Design',
-      'Mobile',
-      'Inovação e Gestão'
-  ]
-
   const [nome, setNome] = useState('')
   const [cargo, setCargo] = useState('')
   const [imagem, setImagem] = useState('')
@@ -61,7 +51,7 @@ const times = [
 
         <ListaSuspensa 
           obrigatorio={true} 
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={valor => setTime(valor)}
         />
